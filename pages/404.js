1c6717e5c7d893d491ac1ac79fb2707abe219ca1
sakhin123 <67752193+sakhin123@users.noreply.github.com>
2022-06-29 15:30:15 -0400

    Added custom 404 page, ready for review (#5)

---
+++ pages/404.js
@@ -0,0 +1,12 @@
+import { Center } from "@mantine/core";
+
+export default function Custom404() {
+  return (
+    <Center>
+      <div>
+        <h1>404 - Page Not Found</h1>
+        Use the navigation buttons to navigate to a valid URL
+      </div>
+    </Center>
+  );
+}
