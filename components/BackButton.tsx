8f8a0da34be522d74b8623967b1ab41920952f46
Sakhi Naik <sakhinaik3@gmail.com>
2022-07-08 13:33:08 -0400

    Commenting BackButton component

--- components/BackButton.tsx
+++ components/BackButton.tsx
@@ -1,6 +1,10 @@
 import { useRouter } from "next/router";
 import { Button } from "@mantine/core";
 
+/**
+ * BackButton is a component for rendering a back button
+ * @returns Button that routes user back when clicked
+ */
 const BackButton = () => {
   const router = useRouter();
   return (
--- pages/[resourceType]/[id].tsx
+++ pages/[resourceType]/[id].tsx
@@ -1,7 +1,7 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { Prism } from "@mantine/prism";
-import { Button, Divider, ScrollArea, Stack } from "@mantine/core";
+import { Divider, ScrollArea, Stack } from "@mantine/core";
 import BackButton from "../../components/BackButton";
 
 /**
