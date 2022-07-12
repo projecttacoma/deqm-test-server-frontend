038445a9cd7cd3f08e39f1bd6aa96c85d3b1b469
sakhin123 <67752193+sakhin123@users.noreply.github.com>
2022-07-12 09:33:01 -0400

    Create resource request (#7)
    * initial push
    
    * create resource post request implementation, ready for review
    
    * addressed initial review comments, ready for second review
    
    * second round Matt comments addressed, ready for another review

--- __tests__/pages/create.test.tsx
+++ __tests__/pages/create.test.tsx
@@ -1,8 +1,13 @@
 import { render, screen, act } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import NewResource from "../../pages/[resourceType]/create";
-import { createMockRouter } from "../helpers/testHelpers";
+import CreateResourcePage from "../../pages/[resourceType]/create";
 import { RouterContext } from "next/dist/shared/lib/router-context";
+import {
+  mantineRecoilWrap,
+  createMockRouter,
+  getMockFetchImplementationError,
+} from "../helpers/testHelpers";
+import userEvent from "@testing-library/user-event";
 
 describe("create new resource page render", () => {
   it("should display a code editor component and a submit resource button", async () => {
@@ -13,11 +18,52 @@ describe("create new resource page render", () => {
             query: { resourceType: "Patient" },
           })}
         >
-          <NewResource />
+          <CreateResourcePage />
         </RouterContext.Provider>,
       );
     });
     expect(await screen.findByRole("button", { name: "Submit Resource" })).toBeInTheDocument();
     expect(await screen.findByTestId("resource-code-editor")).toBeInTheDocument();
+  });
+});
+
+/*
+  An issue arose when trying to mock user input of valid JSON into the code editor in order to enable the
+  submit button for unit testing. So, testing of the create resource fetch implementation needs further investigation because 
+  the behavior of the ResourceCodeEditor and submit button in a unit test diverges from that of the app in a browser.
+*/
+describe.skip("error response test", () => {
+  beforeAll(() => {
+    global.fetch = getMockFetchImplementationError("Problem connecting to server");
+  });
+
+  it("should show error notification when not connected to server", async () => {
+    const user = userEvent.setup();
+    await act(async () => {
+      render(
+        mantineRecoilWrap(
+          <RouterContext.Provider
+            value={createMockRouter({
+              query: { resourceType: "Patient" },
+            })}
+          >
+            <CreateResourcePage />
+          </RouterContext.Provider>,
+        ),
+      );
+    });
+
+    const submitButton = screen.getByRole("button", {
+      name: "Submit Resource",
+    }) as HTMLButtonElement;
+
+    const codeEditor = screen.getByRole("textbox");
+
+    await act(async () => {
+      user.type(codeEditor, "{}");
+      user.click(submitButton);
+    });
+
+    expect((await screen.findByRole("alert")) as HTMLDivElement).toBeInTheDocument();
   });
 });
--- pages/[resourceType]/create.tsx
+++ pages/[resourceType]/create.tsx
@@ -3,8 +3,13 @@ import React, { useState } from "react";
 import ResourceCodeEditor from "../../components/ResourceCodeEditor";
 import { Button, Center, Text } from "@mantine/core";
 import { cyanMainShade } from "../_app";
+import { cleanNotifications, showNotification, NotificationProps } from "@mantine/notifications";
+import Link from "next/link";
+import { Check, X } from "tabler-icons-react";
 /**
  * CreateResourcePage is a page that renders a code editor and submit button for creating resources.
+ * When the submit button is clicked, a POST request is sent to the test server. If the request is
+ * successful, a success notification with the new ID as a Link appears. Otherwise, an error notification appears.
  * @returns React node with a ResourceCodeEditor component and a submit Button
  */
 const CreateResourcePage = () => {
@@ -12,12 +17,7 @@ const CreateResourcePage = () => {
   const { resourceType } = router.query;
   const [codeEditorContents, setCodeEditorContents] = useState("");
   const [hasLintError, setHasLintError] = useState(true);
-
-  //handles what will happen once the submit button is clicked.
-  //For now, it console.logs the contents of the ResourceCodeEditor
-  function editorSubmitHandler() {
-    console.log(codeEditorContents);
-  }
+  const NEW_ID_IN_HEADER_REGEX = new RegExp(`${resourceType}/[A-Za-z0-9\-\.]{1,64}`);
 
   return (
     <div>
@@ -49,6 +49,92 @@ const CreateResourcePage = () => {
       </Center>
     </div>
   );
+
+  //called when the submit button is clicked. Handles POST request and response
+  async function editorSubmitHandler() {
+    let customMessage: NotificationProps["message"] = <div>Problem connecting to server</div>;
+    let notifProps: NotificationProps = {
+      message: customMessage,
+      color: "red",
+      icon: <X size={18} />,
+      autoClose: false,
+    };
+
+    fetch(`${process.env.NEXT_PUBLIC_DEQM_SERVER}/${resourceType}`, {
+      method: "POST",
+      body: codeEditorContents,
+      headers: {
+        "Content-Type": "application/json+fhir",
+      },
+    })
+      .then((response) => {
+        let newID;
+
+        if (response.status === 201 || response.status === 200) {
+          const regexResponse = NEW_ID_IN_HEADER_REGEX.exec(
+            response.headers.get("Location") as string,
+          );
+
+          if (regexResponse == null) {
+            throw {
+              name: "RegexError",
+              message:
+                "Invalid regexResponse: couldn't find resource ID in response header.\nLocation Header: " +
+                response.headers.get("Location"),
+            };
+          }
+
+          newID = regexResponse[0];
+          customMessage = (
+            <>
+              <Text>Resource successfully created:&nbsp;</Text>
+              <Link href={`/${newID}`} key={`${newID}`} passHref>
+                <Text component="a" color="cyan">
+                  {newID}
+                </Text>
+              </Link>
+            </>
+          );
+          notifProps = {
+            ...notifProps,
+            color: "green",
+            icon: <Check size={18} />,
+          };
+
+          //redirects user to the resourceType home page
+          router.push({ pathname: `/${resourceType}` });
+        } else {
+          customMessage = `${response.status} ${response.statusText}`;
+          return response.json();
+        }
+      })
+      .then((responseBody) => {
+        if (responseBody) {
+          customMessage = (
+            <>
+              <Text weight={500}>{customMessage}&nbsp;</Text>
+              <Text color="red">{responseBody.issue[0].details.text}</Text>
+            </>
+          );
+        }
+      })
+      .catch((error) => {
+        if (error.name === "RegexError") {
+          customMessage = <Text color="red">{error.message}</Text>;
+        } else {
+          customMessage = (
+            <>
+              <Text weight={500}>Problem connecting to server:&nbsp;</Text>
+              <Text color="red">{error.message}</Text>
+            </>
+          );
+        }
+      })
+      .finally(() => {
+        cleanNotifications();
+        showNotification({ ...notifProps, message: customMessage });
+      });
+  }
 };
 
 export default CreateResourcePage;
