8993bfaf09d1bc9b48a667c99c76e3e9754dbb2d
Sakhi Naik <sakhinaik3@gmail.com>
2022-07-14 09:29:28 -0400

    ready for review

--- __tests__/pages/resource/create.test.tsx
+++ __tests__/pages/resource/create.test.tsx
@@ -7,10 +7,13 @@ import {
   createMockRouter,
   getMockFetchImplementationError,
   createRectRange,
+  getMockFetchImplementation,
 } from "../../helpers/testHelpers";
 import userEvent from "@testing-library/user-event";
 
-describe.skip("create new resource page render", () => {
+const ERROR_400_RESPONSE_BODY = { issue: [{ details: { text: "Invalid resource body" } }] };
+
+describe("Create new resource page render", () => {
   it("should display a code editor component, submit resource button, and a back button", async () => {
     await act(async () => {
       render(
@@ -23,19 +26,106 @@ describe.skip("create new resource page render", () => {
         </RouterContext.Provider>,
       );
     });
-    expect(await screen.findByTestId("back-button")).toBeInTheDocument();
-    expect(await screen.findByRole("button", { name: "Submit Resource" })).toBeInTheDocument();
-    expect(await screen.findByTestId("resource-code-editor")).toBeInTheDocument();
+    expect(screen.getByTestId("back-button")).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: "Submit Resource" })).toBeInTheDocument();
+    expect(screen.getByTestId("resource-code-editor")).toBeInTheDocument();
   });
 });
 
-describe("error response test", () => {
+describe("Successful resource creation", () => {
+  beforeAll(() => {
+    global.fetch = getMockFetchImplementation(
+      "",
+      200,
+      "Resource created!!",
+      new Headers({ Location: "4_0_1/DiagnosticReport/123456789" }),
+    );
+    document.createRange = createRectRange;
+  });
+
+  it("Test for success notification", async () => {
+    const user = userEvent.setup();
+    await act(async () => {
+      render(
+        mantineRecoilWrap(
+          <RouterContext.Provider
+            value={createMockRouter({
+              query: { resourceType: "DiagnosticReport" },
+            })}
+          >
+            <CreateResourcePage />
+          </RouterContext.Provider>,
+        ),
+      );
+    });
+
+    const submitButton = screen.getByRole("button", {
+      name: "Submit Resource",
+    }) as HTMLButtonElement;
+
+    const codeEditor = screen.getByRole("textbox");
+
+    //CodeMirror autocloses brackets, so only one is necessary to type
+    user.type(codeEditor, "{{").finally(() => {
+      //await user.Type Promise to resolve/reject before clicking the submit button
+      user.click(submitButton);
+    });
+
+    const errorNotif = (await screen.findByRole("alert")) as HTMLDivElement;
+    expect(errorNotif).toBeInTheDocument();
+
+    expect(within(errorNotif).getByText(/Resource successfully created:/)).toBeInTheDocument();
+    expect(within(errorNotif).getByText(/DiagnosticReport\/123456789/)).toBeInTheDocument();
+  });
+});
+
+describe("Invalid resource creation", () => {
+  beforeAll(() => {
+    global.fetch = getMockFetchImplementation(ERROR_400_RESPONSE_BODY, 400, "Bad Request");
+    document.createRange = createRectRange;
+  });
+
+  it("Test for error notification for 400 response", async () => {
+    const user = userEvent.setup();
+    await act(async () => {
+      render(
+        mantineRecoilWrap(
+          <RouterContext.Provider
+            value={createMockRouter({
+              query: { resourceType: "DiagnosticReport" },
+            })}
+          >
+            <CreateResourcePage />
+          </RouterContext.Provider>,
+        ),
+      );
+    });
+
+    const submitButton = screen.getByRole("button", {
+      name: "Submit Resource",
+    }) as HTMLButtonElement;
+
+    const codeEditor = screen.getByRole("textbox");
+
+    user.type(codeEditor, "{{").finally(() => {
+      user.click(submitButton);
+    });
+
+    const errorNotif = (await screen.findByRole("alert")) as HTMLDivElement;
+    expect(errorNotif).toBeInTheDocument();
+
+    expect(within(errorNotif).getByText(/400 Bad Request/)).toBeInTheDocument();
+    expect(within(errorNotif).getByText(/Invalid resource body/)).toBeInTheDocument();
+  });
+});
+
+describe("Error thrown during create test", () => {
   beforeAll(() => {
     global.fetch = getMockFetchImplementationError("400 Bad Request");
     document.createRange = createRectRange;
   });
 
-  it("should show error notification when not connected to server", async () => {
+  it("Test for error notification when error is thrown", async () => {
     const user = userEvent.setup();
     await act(async () => {
       render(
@@ -57,15 +147,7 @@ describe("error response test", () => {
 
     const codeEditor = screen.getByRole("textbox");
 
-    await act(async () => {
-      user.type(codeEditor, "{{");
-    });
-
-    const doneTyping = new Promise((resolve, reject) => {
-      setTimeout(() => resolve("value"), 500);
-    });
-    doneTyping.finally(() => {
-      console.log("clicking button");
+    user.type(codeEditor, "{{").finally(() => {
       user.click(submitButton);
     });
 
