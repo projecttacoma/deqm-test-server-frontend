e5732bc4d46a37fa9f496762ae886c5db6af5562
Sakhi Naik <sakhinaik3@gmail.com>
2022-07-21 15:02:22 -0400

    pre-filled datePickers

--- pages/[resourceType]/[id]/evaluate.tsx
+++ pages/[resourceType]/[id]/evaluate.tsx
@@ -1,8 +1,14 @@
-import { Center, Text } from "@mantine/core";
+import { Center, Divider, Grid, Text } from "@mantine/core";
 import { DatePicker } from "@mantine/dates";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
+import { Calendar } from "tabler-icons-react";
 import { cleanNotifications, showNotification } from "@mantine/notifications";
+import { DateTime } from "luxon";
+import { textGray } from "../../../styles/appColors";
+
+const DEFAULT_PERIOD_START = new Date(`${DateTime.now().year}-01-01T00:00:00`);
+const DEFAULT_PERIOD_END = new Date(`${DateTime.now().year}-12-31T00:00:00`);
 
 /**
  * Page for evaluate measure functionality.
@@ -22,12 +28,16 @@ const EvaluateMeasurePage = () => {
           return data.json();
         })
         .then((resourcePageBody) => {
-          const date1 = resourcePageBody.effectivePeriod.start;
-          const date2 = resourcePageBody.effectivePeriod.end;
-          console.log("date1-date2: ", date1, date2);
-          setPeriodStart(date1);
-          setPeriodEnd(date2);
-          console.log("stateDate1-stateDate2: ", periodStart, periodEnd);
+          setPeriodStart(
+            resourcePageBody.effectivePeriod.start
+              ? new Date(DateTime.fromISO(resourcePageBody.effectivePeriod.start).toISO())
+              : DEFAULT_PERIOD_START,
+          );
+          setPeriodEnd(
+            resourcePageBody.effectivePeriod.end
+              ? new Date(DateTime.fromISO(resourcePageBody.effectivePeriod.end).toISO())
+              : DEFAULT_PERIOD_END,
+          );
           setFetchingError(false);
         })
         .catch((error) => {
@@ -41,18 +51,36 @@ const EvaluateMeasurePage = () => {
           });
         });
     }
-  });
+  }, [id]);
+
   if (!fetchingError) {
     if (resourceType === "Measure") {
       return (
         <>
-          <h2>Evaluate Measure: {id}</h2>
-          <DatePicker
-            label="periodStart"
-            value={periodStart}
-            onChange={setPeriodStart}
-          ></DatePicker>
-          <DatePicker label="periodEnd" value={periodEnd} onChange={setPeriodEnd}></DatePicker>
+          <Center>
+            <h2 style={{ color: textGray, marginTop: "2px", marginBottom: "1px" }}>
+              Evaluate Measure: {id}
+            </h2>
+          </Center>
+          <Divider my="md" />
+          <Grid gutter="lg" style={{ margin: 10 }}>
+            <Grid.Col span={2}>
+              <DatePicker
+                label={<Text>Period Start</Text>}
+                icon={<Calendar size={16} color={"#40a5bf"} />}
+                value={periodStart}
+                onChange={setPeriodStart}
+              ></DatePicker>
+            </Grid.Col>
+            <Grid.Col span={2}>
+              <DatePicker
+                label={<Text>Period End</Text>}
+                icon={<Calendar size={16} color={"#40a5bf"} />}
+                value={periodEnd}
+                onChange={setPeriodEnd}
+              ></DatePicker>
+            </Grid.Col>
+          </Grid>
         </>
       );
     } else {
@@ -64,6 +92,8 @@ const EvaluateMeasurePage = () => {
         </Center>
       );
     }
+  } else {
+    return <div>Something went wrong.</div>;
   }
 };
 
