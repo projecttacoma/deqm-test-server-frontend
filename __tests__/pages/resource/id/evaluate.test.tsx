fded7a8fd97d938a3a5af94a29e11f4962c13858
Sakhi Naik <sakhinaik3@gmail.com>
2022-07-22 11:31:15 -0400

    DatePicker implementation done, unit testing done

--- __tests__/pages/resource/id/evaluate.test.tsx
+++ __tests__/pages/resource/id/evaluate.test.tsx
@@ -1,16 +1,81 @@
-import { render, screen, act } from "@testing-library/react";
+import { render, screen, act, fireEvent, within } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import { createMockRouter } from "../../../helpers/testHelpers";
+import {
+  mantineRecoilWrap,
+  createMockRouter,
+  getMockFetchImplementation,
+} from "../../../helpers/testHelpers";
 import { RouterContext } from "next/dist/shared/lib/router-context";
 import EvaluateMeasurePage from "../../../../pages/[resourceType]/[id]/evaluate";
+import { DateTime } from "luxon";
+
+const MEASURE_BODY_WITH_DATES = {
+  resourceType: "Measure",
+  id: "measure-EXM104-8.2.000",
+  meta: {
+    lastUpdated: "2022-07-21T18:12:25.008Z",
+  },
+  effectivePeriod: {
+    start: "2019-01-01",
+    end: "2019-12-31",
+  },
+};
+
+const MEASURE_BODY_NO_DATES = {
+  resourceType: "Measure",
+  id: "measure-EXM104-8.4.000",
+  meta: {
+    lastUpdated: "2022-07-21T18:12:25.008Z",
+  },
+  effectivePeriod: {},
+};
+
+const ERROR_400_RESPONSE_BODY = { issue: [{ details: { text: "Invalid resource ID" } }] };
 
 describe("Test evaluate page render for measure", () => {
-  it("should display expected text", async () => {
+  beforeAll(() => {
+    global.fetch = getMockFetchImplementation(MEASURE_BODY_WITH_DATES);
+  });
+
+  it("should display back button and expected title", async () => {
     await act(async () => {
       render(
         <RouterContext.Provider
           value={createMockRouter({
-            query: { resourceType: "Measure", id: "Measure-12" },
+            query: { resourceType: "Measure", id: "measure-EXM104-8.2.000" },
+          })}
+        >
+          <EvaluateMeasurePage />
+        </RouterContext.Provider>,
+      );
+    });
+    expect(screen.getByTestId("back-button")).toBeInTheDocument();
+    expect(screen.getByText("Evaluate Measure: measure-EXM104-8.2.000")).toBeInTheDocument();
+  });
+
+  it("should display two DatePickers pre-filled with expected dates", async () => {
+    await act(async () => {
+      render(
+        <RouterContext.Provider
+          value={createMockRouter({
+            query: { resourceType: "Measure", id: "measure-EXM104-8.2.000" },
+          })}
+        >
+          <EvaluateMeasurePage />
+        </RouterContext.Provider>,
+      );
+    });
+    //DatePickers should pre-fill with the effective period dates from the Measure
+    expect(screen.getByDisplayValue("January 1, 2019")).toBeInTheDocument();
+    expect(screen.getByDisplayValue("December 31, 2019")).toBeInTheDocument();
+  });
+
+  it("DatePickers display value should changed when value is updated", async () => {
+    await act(async () => {
+      render(
+        <RouterContext.Provider
+          value={createMockRouter({
+            query: { resourceType: "Measure", id: "measure-EXM104-8.2.000" },
           })}
         >
           <EvaluateMeasurePage />
@@ -18,12 +83,46 @@ describe("Test evaluate page render for measure", () => {
       );
     });
 
-    expect(screen.getByText(/Coming soon.../)).toBeInTheDocument();
+    //DatePickers should change when their value is altered
+    const periodStartSelector = screen.getByLabelText("Period Start");
+    const periodEndSelector = screen.getByLabelText("Period End");
+    await act(async () => {
+      fireEvent.change(periodStartSelector, {
+        target: { value: DateTime.fromISO("2018-02-02").toJSDate() },
+      });
+      fireEvent.change(periodEndSelector, {
+        target: { value: DateTime.fromISO("2020-11-13").toJSDate() },
+      });
+    });
+    expect(screen.getByDisplayValue("February 2, 2018")).toBeInTheDocument();
+    expect(screen.getByDisplayValue("November 13, 2020")).toBeInTheDocument();
+  });
+});
+
+describe("Test evaluate page render for measure without effective period dates", () => {
+  beforeAll(() => {
+    global.fetch = getMockFetchImplementation(MEASURE_BODY_NO_DATES);
+  });
+
+  it("should display DatePickers with default dates", async () => {
+    await act(async () => {
+      render(
+        <RouterContext.Provider
+          value={createMockRouter({
+            query: { resourceType: "Measure", id: "measure-EXM104-8.4.000" },
+          })}
+        >
+          <EvaluateMeasurePage />
+        </RouterContext.Provider>,
+      );
+    });
+    expect(await screen.findByDisplayValue("January 1, 2022")).toBeInTheDocument();
+    expect(screen.getByDisplayValue("December 31, 2022")).toBeInTheDocument();
   });
 });
 
 describe("Test evaluate page render for non-measure", () => {
-  it("should display an error message", async () => {
+  it("should display an error message and back button", async () => {
     await act(async () => {
       render(
         <RouterContext.Provider
@@ -36,10 +135,39 @@ describe("Test evaluate page render for non-measure", () => {
       );
     });
 
+    expect(screen.getByTestId("back-button")).toBeInTheDocument();
     expect(
       screen.getByText(
         /Cannot evaluate on resourceType: DiagnosticReport, only on resourceType: Measure/,
       ),
     ).toBeInTheDocument();
+  });
+});
+
+describe("non 20x response in evaluate measure page", () => {
+  beforeEach(() => {
+    global.fetch = getMockFetchImplementation(ERROR_400_RESPONSE_BODY, 400, "BadRequest");
+  });
+
+  it("error notification should appear with expected messages", async () => {
+    await act(async () => {
+      render(
+        mantineRecoilWrap(
+          <RouterContext.Provider
+            value={createMockRouter({
+              query: { resourceType: "Measure", id: "measure-EXM104-8.4.000" },
+            })}
+          >
+            <EvaluateMeasurePage />
+          </RouterContext.Provider>,
+        ),
+      );
+    });
+
+    const errorNotif = (await screen.findByRole("alert")) as HTMLDivElement;
+    expect(errorNotif).toBeInTheDocument();
+
+    expect(within(errorNotif).getByText(/400 BadRequest/)).toBeInTheDocument();
+    expect(within(errorNotif).getByText(/Invalid resource ID/)).toBeInTheDocument();
   });
 });
--- pages/[resourceType]/[id]/evaluate.tsx
+++ pages/[resourceType]/[id]/evaluate.tsx
@@ -2,17 +2,20 @@ import { Center, Divider, Grid, Text } from "@mantine/core";
 import { DatePicker } from "@mantine/dates";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
-import { Calendar } from "tabler-icons-react";
+import { Calendar, X } from "tabler-icons-react";
 import { cleanNotifications, showNotification } from "@mantine/notifications";
 import { DateTime } from "luxon";
 import { textGray } from "../../../styles/appColors";
+import BackButton from "../../../components/BackButton";
 
 const DEFAULT_PERIOD_START = new Date(`${DateTime.now().year}-01-01T00:00:00`);
 const DEFAULT_PERIOD_END = new Date(`${DateTime.now().year}-12-31T00:00:00`);
 
 /**
- * Page for evaluate measure functionality.
- * @returns JSX Element
+ * EvaluateMeasurePage is a page that renders a back button and DatePickers that are pre-filled with
+ * a Measure's effective period dates or default dates.
+ * If the url resourceType is not a Measure, an error message is displayed.
+ * @returns React node with a back button and DatePickers if on a valid Measure url
  */
 const EvaluateMeasurePage = () => {
   const router = useRouter();
@@ -22,23 +25,43 @@ const EvaluateMeasurePage = () => {
   const [periodEnd, setPeriodEnd] = useState<Date | null>(new Date());
   useEffect(() => {
     if (resourceType === "Measure" && id) {
-      //fetch the resource JSON content from the test server based on resource and id from url
+      let fetchStatus = { status: 500, statusText: "Failed fetch request" };
+      //fetch the resource JSON content from the test server based on resourceType and id from url
       fetch(`${process.env.NEXT_PUBLIC_DEQM_SERVER}/${resourceType}/${id}`)
         .then((data) => {
+          fetchStatus = { status: data.status, statusText: data.statusText };
           return data.json();
         })
         .then((resourcePageBody) => {
-          setPeriodStart(
-            resourcePageBody.effectivePeriod.start
-              ? new Date(DateTime.fromISO(resourcePageBody.effectivePeriod.start).toISO())
-              : DEFAULT_PERIOD_START,
-          );
-          setPeriodEnd(
-            resourcePageBody.effectivePeriod.end
-              ? new Date(DateTime.fromISO(resourcePageBody.effectivePeriod.end).toISO())
-              : DEFAULT_PERIOD_END,
-          );
-          setFetchingError(false);
+          if (fetchStatus.status === 200 || fetchStatus.status === 201) {
+            setPeriodStart(
+              resourcePageBody?.effectivePeriod?.start
+                ? new Date(DateTime.fromISO(resourcePageBody.effectivePeriod.start).toISO())
+                : DEFAULT_PERIOD_START,
+            );
+            setPeriodEnd(
+              resourcePageBody?.effectivePeriod?.end
+                ? new Date(DateTime.fromISO(resourcePageBody.effectivePeriod.end).toISO())
+                : DEFAULT_PERIOD_END,
+            );
+            setFetchingError(false);
+          } else {
+            setFetchingError(true);
+            cleanNotifications();
+            showNotification({
+              message: (
+                <>
+                  <Text weight={500}>
+                    {fetchStatus.status} {fetchStatus.statusText}:&nbsp;
+                  </Text>
+                  <Text color="red">{resourcePageBody.issue[0].details.text}</Text>
+                </>
+              ),
+              color: "red",
+              icon: <X size={18} />,
+              autoClose: false,
+            });
+          }
         })
         .catch((error) => {
           setFetchingError(true);
@@ -47,53 +70,72 @@ const EvaluateMeasurePage = () => {
           showNotification({
             message: "Not connected to server!",
             color: "red",
+            icon: <X size={18} />,
             autoClose: false,
           });
         });
     }
-  }, [id]);
+  }, [resourceType, id]);
 
   if (!fetchingError) {
     if (resourceType === "Measure") {
+      //for resourceType Measure, evaluate measure components are rendered
       return (
         <>
+          <BackButton />
           <Center>
-            <h2 style={{ color: textGray, marginTop: "2px", marginBottom: "1px" }}>
+            <h2 style={{ color: textGray, marginTop: "0px", marginBottom: "4px" }}>
               Evaluate Measure: {id}
             </h2>
           </Center>
           <Divider my="md" />
-          <Grid gutter="lg" style={{ margin: 10 }}>
-            <Grid.Col span={2}>
+          <Grid gutter="lg" min-width="15px" style={{ margin: 10 }}>
+            <Grid.Col md={4} lg={3} xl={2}>
               <DatePicker
+                data-testid="period-start-datepicker"
                 label={<Text>Period Start</Text>}
                 icon={<Calendar size={16} color={"#40a5bf"} />}
                 value={periodStart}
                 onChange={setPeriodStart}
+                allowFreeInput
               ></DatePicker>
             </Grid.Col>
-            <Grid.Col span={2}>
+            <Grid.Col md={4} lg={3} xl={2}>
               <DatePicker
+                data-testid="period-end-datepicker"
                 label={<Text>Period End</Text>}
                 icon={<Calendar size={16} color={"#40a5bf"} />}
                 value={periodEnd}
                 onChange={setPeriodEnd}
+                allowFreeInput
               ></DatePicker>
             </Grid.Col>
           </Grid>
         </>
       );
     } else {
+      //if resourceType is not a Measure, an error message is displayed
       return (
-        <Center>
-          <div>
-            Cannot evaluate on resourceType: {`${resourceType}`}, only on resourceType: Measure
-          </div>
-        </Center>
+        <>
+          <BackButton />
+          <Center>
+            <div>
+              Cannot evaluate on resourceType: {`${resourceType}`}, only on resourceType: Measure
+            </div>
+          </Center>
+        </>
       );
     }
   } else {
-    return <div>Something went wrong.</div>;
+    //if fetch request throws an error, an error message is displayed
+    return (
+      <>
+        <BackButton />
+        <Center>
+          <div>Something went wrong.</div>
+        </Center>
+      </>
+    );
   }
 };
 
