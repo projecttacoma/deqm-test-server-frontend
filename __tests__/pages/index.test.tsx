c8e630289dbc1465d30a41f4e66fa3e31c23d2af
Sakhi Naik <sakhinaik3@gmail.com>
2022-07-22 13:18:33 -0400

    ready for merge

--- __tests__/pages/index.test.tsx
+++ __tests__/pages/index.test.tsx
@@ -4,7 +4,7 @@ import Home from "../../pages/index";
 import { mantineRecoilWrap } from "../helpers/testHelpers";
 
 describe("Test Home page rendering", () => {
-  it("should display an Upload Transaction Bundle button", async () => {
+  it("should display an Upload Transaction Bundle button", () => {
     render(mantineRecoilWrap(<Home></Home>));
     expect(screen.getByRole("link", { name: "Upload Transaction Bundle" })).toBeInTheDocument();
   });
