16d7e400f69a4ae394eb270b04aa0493882c6059
Dana <djacoby@mitre.org>
2022-07-24 19:19:07 -0400

    minor changes to make label reusable with different resource types

--- components/SelectComponent.tsx
+++ components/SelectComponent.tsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 import { Autocomplete } from "@mantine/core";
 
 /**
- * @param props.resourceType
+ * @param props include the string resourceType
  * @returns a component with a loading component, server error, or autocomplete select component populated with resource IDs
  */
 export default function SelectComponent(props) {
@@ -35,7 +35,7 @@ export default function SelectComponent(props) {
   return loadingRequest ? (
     <div>Loading content...</div>
   ) : !fetchingError && pageBody ? (
-    <PopulateIDHelper jsonBody={pageBody}></PopulateIDHelper>
+    <PopulateIDHelper jsonBody={pageBody} resourceType={resourceType}></PopulateIDHelper>
   ) : (
     <div>Problem connecting to server</div>
   );
@@ -45,11 +45,11 @@ export default function SelectComponent(props) {
  * @param props.jsonBody: fhirJson.Bundle
  * @returns a component with an error message that resources don't exist or autocomplete select component populated with resource IDs
  */
-function PopulateIDHelper(props: { jsonBody: fhirJson.Bundle }) {
+function PopulateIDHelper(props: { jsonBody: fhirJson.Bundle; resourceType: string }) {
   const entryArray = props.jsonBody.entry;
   console.log(entryArray);
   if (props.jsonBody.total && props.jsonBody.total > 0 && entryArray != undefined) {
-    return PopulateSelect(entryArray);
+    return PopulateSelect(entryArray, props.resourceType);
   } else {
     return <div> No resources found </div>;
   }
@@ -59,7 +59,7 @@ function PopulateIDHelper(props: { jsonBody: fhirJson.Bundle }) {
  * @param fhirJson.BundleEntry
  * @returns an autocomplete select component populated with resource IDs
  */
-const PopulateSelect = (entry: (fhirJson.BundleEntry | null)[]) => {
+const PopulateSelect = (entry: (fhirJson.BundleEntry | null)[], resourceType: string) => {
   const myArray = entry.map((el) => {
     return el?.resource ? `${el.resource.resourceType}/${el.resource.id}` : "";
   });
@@ -69,7 +69,7 @@ const PopulateSelect = (entry: (fhirJson.BundleEntry | null)[]) => {
     <Autocomplete
       value={value}
       onChange={setValue}
-      label="Select Practitioner"
+      label={`Select ${resourceType}`}
       placeholder="Start typing to see options"
       data={myArray}
       limit={10}
