d8fcb6d798f40ee051ac6e69b66b2256d1abd3c3
Sakhi Naik <sakhinaik3@gmail.com>
2022-08-01 13:05:17 -0400

    careGaps unit testing in progress

---
+++ __tests__/pages/resource/id/careGaps.test.tsx
@@ -0,0 +1,195 @@
+import { render, screen, act, fireEvent, within } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import {
+  mantineRecoilWrap,
+  createMockRouter,
+  getMockFetchImplementation,
+  getMockFetchImplementationError,
+  mockResizeObserver,
+} from "../../../helpers/testHelpers";
+import { RouterContext } from "next/dist/shared/lib/router-context";
+import CareGapsPage from "../../../../pages/[resourceType]/[id]/careGaps";
+import { DateTime } from "luxon";
+import { fhirJson } from "@fhir-typescript/r4-core";
+
+const MEASURE_BODY_WITH_DATES = {
+  resourceType: "Measure",
+  id: "measure-EXM104-8.2.000",
+  meta: {
+    lastUpdated: "2022-07-21T18:12:25.008Z",
+  },
+  effectivePeriod: {
+    start: "2019-01-01",
+    end: "2019-12-31",
+  },
+};
+
+const RESOURCE_ID_BODY: fhirJson.Bundle = {
+  resourceType: "Bundle",
+  meta: {
+    lastUpdated: "2022-06-23T19:52:58.721Z",
+  },
+  type: "searchset",
+  total: 2,
+  entry: [
+    {
+      fullUrl: "http://localhost:3000/4_0_1/PractitionerReport/denom-EXM125-3",
+      resource: {
+        resourceType: "Practitioner",
+        id: "denom-EXM125-3",
+        meta: {
+          profile: [
+            "http://hl7.org/fhir/us/core/StructureDefinition/us-core-practitionerreport-note",
+          ],
+        },
+      },
+    },
+    {
+      fullUrl: "http://localhost:3000/4_0_1/PractitionerReport/numer-EXM125-3",
+      resource: {
+        resourceType: "Practitioner",
+        id: "numer-EXM125-3",
+        meta: {
+          profile: [
+            "http://hl7.org/fhir/us/core/StructureDefinition/us-core-practitionerreport-note",
+          ],
+        },
+      },
+    },
+  ],
+};
+
+describe("Test evaluate page render for measure", () => {
+  beforeAll(() => {
+    global.fetch = getMockFetchImplementation(MEASURE_BODY_WITH_DATES);
+  });
+
+  it("should display back button, expected title, two pre-filled DatePickers, and disabled calculate button", async () => {
+    await act(async () => {
+      render(
+        <RouterContext.Provider
+          value={createMockRouter({
+            query: { resourceType: "Measure", id: "measure-EXM104-8.2.000" },
+          })}
+        >
+          <CareGapsPage />
+        </RouterContext.Provider>,
+      );
+    });
+    expect(screen.getByTestId("back-button")).toBeInTheDocument();
+    expect(screen.getByText("Calculate Care Gaps: measure-EXM104-8.2.000")).toBeInTheDocument();
+
+    //DatePickers should pre-fill with the effective period dates from the Measure
+    expect(screen.getByDisplayValue("January 1, 2019")).toBeInTheDocument();
+    expect(screen.getByDisplayValue("December 31, 2019")).toBeInTheDocument();
+  });
+
+  it("DatePickers and request preview display value should changed when dates are updated", async () => {
+    await act(async () => {
+      render(
+        <RouterContext.Provider
+          value={createMockRouter({
+            query: { resourceType: "Measure", id: "measure-EXM104-8.2.000" },
+          })}
+        >
+          <CareGapsPage />
+        </RouterContext.Provider>,
+      );
+    });
+
+    //DatePickers should change when their value is altered
+    const periodStartSelector = screen.getByLabelText("Period Start");
+    const periodEndSelector = screen.getByLabelText("Period End");
+    await act(async () => {
+      fireEvent.change(periodStartSelector, {
+        target: { value: DateTime.fromISO("2018-02-02").toJSDate() },
+      });
+      fireEvent.change(periodEndSelector, {
+        target: { value: DateTime.fromISO("2020-11-13").toJSDate() },
+      });
+    });
+    expect(screen.getByDisplayValue("February 2, 2018")).toBeInTheDocument();
+    expect(screen.getByDisplayValue("November 13, 2020")).toBeInTheDocument();
+  });
+});
+
+describe("Test evaluate page render for non-measure", () => {
+  it("should display an error message", async () => {
+    await act(async () => {
+      render(
+        <RouterContext.Provider
+          value={createMockRouter({
+            query: { resourceType: "DiagnosticReport", id: "denom-EXM125-3" },
+          })}
+        >
+          <CareGapsPage />
+        </RouterContext.Provider>,
+      );
+    });
+
+    expect(screen.getByTestId("back-button")).toBeInTheDocument();
+    expect(
+      screen.getByText(
+        /Cannot calculate care gaps on resourceType: DiagnosticReport, only on resourceType: Measure/,
+      ),
+    ).toBeInTheDocument();
+  });
+});
+
+describe("Input/Select components, Radio buttons", () => {
+  beforeAll(() => {
+    global.fetch = getMockFetchImplementation(RESOURCE_ID_BODY);
+  });
+  window.ResizeObserver = mockResizeObserver;
+
+  it("tests for expected enabled/disabled state of input/select components", async () => {
+    await act(async () => {
+      render(
+        <RouterContext.Provider
+          value={createMockRouter({
+            query: { resourceType: "Measure", id: "Measure-12" },
+          })}
+        >
+          <CareGapsPage />
+        </RouterContext.Provider>,
+      );
+    });
+    // Subject radio button should be pre-selected, so Select Patient component should be enabled
+    const subjectRadio = screen.getByLabelText("Subject");
+    expect(subjectRadio).toBeChecked();
+    expect(screen.getByRole("searchbox", { name: "Select Patient" })).not.toBeDisabled();
+
+    //other AutoComplete components should be disabled, Program text input should be enabled
+    expect(screen.getByRole("searchbox", { name: "Select Organization" })).toBeDisabled();
+    expect(screen.getByRole("searchbox", { name: "Select Practitioner" })).toBeDisabled();
+    expect(screen.getByRole("textbox", { name: "Program" })).not.toBeDisabled();
+  });
+
+  it("tests for expected enabled/disabled state of input/select components", async () => {
+    await act(async () => {
+      render(
+        <RouterContext.Provider
+          value={createMockRouter({
+            query: { resourceType: "Measure", id: "Measure-12" },
+          })}
+        >
+          <CareGapsPage />
+        </RouterContext.Provider>,
+      );
+    });
+    //click the organization radio button to ensure the Patient autocomplete component doesn't appear
+    const organizationRadio = screen.getByLabelText("Organization");
+    //Organization radio button should not be pre-selected
+    expect(organizationRadio).not.toBeChecked();
+    await act(async () => {
+      fireEvent.click(organizationRadio);
+    });
+
+    expect(screen.getByRole("searchbox", { name: "Select Patient" })).toBeDisabled();
+
+    //other AutoComplete components and Program text input should all be enabled
+    expect(screen.getByRole("searchbox", { name: "Select Organization" })).not.toBeDisabled();
+    expect(screen.getByRole("searchbox", { name: "Select Practitioner" })).not.toBeDisabled();
+    expect(screen.getByRole("textbox", { name: "Program" })).not.toBeDisabled();
+  });
+});
--- pages/[resourceType]/[id]/careGaps.tsx
+++ pages/[resourceType]/[id]/careGaps.tsx
@@ -1,4 +1,3 @@
-import { useRouter } from "next/router";
 import {
   Center,
   Divider,
@@ -9,28 +8,266 @@ import {
   Loader,
   ScrollArea,
   MantineProvider,
+  TextInput,
 } from "@mantine/core";
-import BackButton from "../../../components/BackButton";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
+import { DateTime } from "luxon";
 import { textGray } from "../../../styles/appColors";
+import BackButton from "../../../components/BackButton";
+import SelectComponent from "../../../components/SelectComponent";
+import MeasureDatePickers from "../../../components/MeasureDatePickers";
+import { Grid } from "@mantine/core";
+import {
+  replaceBackground,
+  replaceOutline,
+  replaceSecondRed,
+} from "../../../styles/codeColorScheme";
+import { Prism } from "@mantine/prism";
+import {
+  replaceDark,
+  replaceGray,
+  replaceTeal,
+  replaceRed,
+  replaceBlue,
+} from "../../../styles/codeColorScheme";
 
-const EvaluateMeasurePage = () => {
+const DEFAULT_PERIOD_START = new Date(`${DateTime.now().year}-01-01T00:00:00`);
+const DEFAULT_PERIOD_END = new Date(`${DateTime.now().year}-12-31T00:00:00`);
+
+const CareGapsPage = () => {
   const router = useRouter();
   const { resourceType, id } = router.query;
+  const [radioValue, setRadioValue] = useState("Subject");
+  const [fetchingError, setFetchingError] = useState(false);
+  const [loadingRequest, setLoadingRequest] = useState(false);
+  const [measureReportBody, setMeasureReportBody] = useState("");
+  const [gridColSpans, setGridColSpans] = useState([3, 3, 0]);
+  const [patientValue, setPatientValue] = useState("");
+  const [practitionerValue, setPractitionerValue] = useState("");
+  const [organizationValue, setOrganizationValue] = useState("");
+  const [periodStart, setPeriodStart] = useState<Date>(DEFAULT_PERIOD_START);
+  const [periodEnd, setPeriodEnd] = useState<Date>(DEFAULT_PERIOD_END);
 
   if (resourceType === "Measure" && id) {
-    //for resourceType Measure, calculate care gaps components are rendered
-    return (
-      <div>
-        <BackButton />
-        <Center>
-          <h2 style={{ color: textGray, marginTop: "0px", marginBottom: "4px" }}>
-            Calculate Care Gaps: {id}
-          </h2>
-        </Center>
-        <Divider my="md" />
-        <Text>Coming soon</Text>
-      </div>
-    );
+    if (!fetchingError) {
+      //for resourceType Measure, calculate care gaps components are rendered
+      return (
+        <div>
+          <BackButton />
+          <Center>
+            <h2 style={{ color: textGray, marginTop: "0px", marginBottom: "4px" }}>
+              Calculate Care Gaps: {id}
+            </h2>
+          </Center>
+          <Divider my="md" />
+          <Grid columns={gridColSpans[0]}>
+            <MantineProvider
+              // changes hex values associated with each Mantine color name to improve UI
+              theme={{
+                colors: {
+                  gray: replaceBackground,
+                  blue: replaceOutline,
+                  red: replaceSecondRed,
+                },
+              }}
+            >
+              <Grid.Col span={gridColSpans[1]}>
+                <Grid.Col>
+                  <Grid.Col style={{ minHeight: 100 }}>
+                    <MeasureDatePickers
+                      measureID={id as string}
+                      periodStart={periodStart}
+                      periodEnd={periodEnd}
+                      startOnUpdate={setPeriodStart}
+                      endOnUpdate={setPeriodEnd}
+                    />
+                  </Grid.Col>
+                  <Grid.Col>
+                    <RadioGroup
+                      value={radioValue}
+                      onChange={setRadioValue}
+                      size="lg"
+                      style={{ marginBottom: "25px" }}
+                    >
+                      <Radio
+                        value="Subject"
+                        label={
+                          <Text size="lg" color={textGray} weight={500}>
+                            Subject
+                          </Text>
+                        }
+                      />
+                      <Radio
+                        value="Organization"
+                        label={
+                          <Text size="lg" color={textGray} weight={500}>
+                            Organization
+                          </Text>
+                        }
+                      />
+                    </RadioGroup>
+                    {radioValue === "Subject" ? (
+                      <SelectComponent
+                        resourceType="Patient"
+                        setValue={setPatientValue}
+                        value={patientValue}
+                        required
+                      />
+                    ) : (
+                      <SelectComponent
+                        resourceType="Patient"
+                        setValue={setPatientValue}
+                        value={patientValue}
+                        disabled
+                        placeholder="Patient selection disabled when 'Organization' is selected"
+                      />
+                    )}
+                  </Grid.Col>
+                  <Grid.Col>
+                    {radioValue === "Organization" ? (
+                      <SelectComponent
+                        resourceType="Organization"
+                        setValue={setOrganizationValue}
+                        value={organizationValue}
+                        required
+                      />
+                    ) : (
+                      <SelectComponent
+                        resourceType="Organization"
+                        setValue={setPatientValue}
+                        value={patientValue}
+                        disabled
+                        placeholder="Organization selection disabled when 'Subject' is selected"
+                      />
+                    )}
+                  </Grid.Col>
+                  <Grid.Col>
+                    {radioValue === "Organization" ? (
+                      <SelectComponent
+                        resourceType="Practitioner"
+                        setValue={setPractitionerValue}
+                        value={practitionerValue}
+                      />
+                    ) : (
+                      <SelectComponent
+                        resourceType="Practitioner"
+                        setValue={setPractitionerValue}
+                        value={practitionerValue}
+                        disabled
+                        placeholder="Practitioner selection disabled when 'Subject' is selected"
+                      />
+                    )}
+                  </Grid.Col>
+                  <Grid.Col>
+                    <TextInput
+                      placeholder="Enter a program"
+                      label="Program"
+                      variant="filled"
+                      radius="xl"
+                      size="lg"
+                    ></TextInput>
+                  </Grid.Col>
+                </Grid.Col>
+                <Grid.Col>
+                  <h3
+                    style={{
+                      color: textGray,
+                      marginTop: "20px",
+                      marginBottom: "2px",
+                      textAlign: "center",
+                    }}
+                  >
+                    Request Preview:
+                  </h3>
+                  <div
+                    style={{
+                      textAlign: "center",
+                      overflowWrap: "break-word",
+                      padding: "10px",
+                      backgroundColor: "#F1F3F5",
+                      border: "1px solid",
+                      borderColor: "#4a4f4f",
+                      borderRadius: "20px",
+                      marginLeft: "30px",
+                      marginRight: "30px",
+                    }}
+                  >
+                    <Text
+                      size="md"
+                      style={{ color: textGray, textAlign: "left" }}
+                    >{`Placeholder`}</Text>
+                  </div>
+                </Grid.Col>
+                <Grid.Col style={{ minHeight: 100 }}>
+                  <div
+                    style={{
+                      textAlign: "center",
+                    }}
+                  >
+                    <MantineProvider
+                      theme={{
+                        colors: {
+                          cyan: replaceOutline,
+                        },
+                      }}
+                    >
+                      <Button
+                        disabled
+                        color="cyan"
+                        radius="md"
+                        size="sm"
+                        variant="filled"
+                        onClick={() => console.log("calculate button clicked")}
+                      >
+                        Calculate
+                      </Button>
+                    </MantineProvider>
+                  </div>
+                </Grid.Col>
+              </Grid.Col>
+              <Grid.Col span={gridColSpans[2]}>
+                {loadingRequest && (
+                  <Center>
+                    <div>Loading content...</div>
+                    <Loader color="cyan"></Loader>
+                  </Center>
+                )}
+                {measureReportBody && !loadingRequest && (
+                  <>
+                    <ScrollArea>
+                      <MantineProvider
+                        //changes hex values associated with each Mantine color name to improve UI
+                        theme={{
+                          colors: {
+                            gray: replaceGray,
+                            dark: replaceDark,
+                            teal: replaceTeal,
+                            red: replaceRed,
+                            blue: replaceBlue,
+                          },
+                        }}
+                      >
+                        <Prism
+                          language="json"
+                          data-testid="prism-measure-report"
+                          colorScheme="dark"
+                          style={{ maxWidth: "77vw", height: "80vh", backgroundColor: "#FFFFFF" }}
+                        >
+                          Placeholder
+                        </Prism>
+                      </MantineProvider>
+                    </ScrollArea>
+                  </>
+                )}
+              </Grid.Col>
+            </MantineProvider>
+          </Grid>
+        </div>
+      );
+    } else {
+      return <div>Something went wrong.</div>;
+    }
   } else {
     return (
       <>
@@ -46,4 +283,4 @@ const EvaluateMeasurePage = () => {
   }
 };
 
-export default EvaluateMeasurePage;
+export default CareGapsPage;
